f2767e47fe7abe13dd7eeaaab343ce43ec984101
Nicholas Edwards <nicholas6598@gmail.com>
2019-08-28 17:35:29 -0400

    Sorts by first letter then checks second incase of same first

--- InsertionSort.java
+++ InsertionSort.java
@@ -45,6 +45,18 @@ public class InsertionSort {
             }
             arr[i + 1] = key;
         }
+
+        int k = 0;
+        for (int j = 1; j < arr.length; j++) {
+            String key = arr[j];
+            int i = j - 1;
+            if(arr[i].charAt(0)==key.charAt(0))
+            while (i >= 0 && arr[i].charAt(1) > key.charAt(1)) {
+                arr[i + 1] = arr[i];
+                i--;
+            }
+            arr[i + 1] = key;
+        }
         return arr;
     }
 }
